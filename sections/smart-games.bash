f2324d2a9938095b3912441f93c78a3a12766224
HughP <HughP@users.noreply.github.com>
2017-02-11 12:58:16 +0100

    Split out of main script

---
+++ sections/smart-games.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+################################################################################
+# Script Name: smart-games.bash
+# Authors: Hugh Paterson III <email here>
+# Version: 0.01
+# License: GPL
+# Dependencies: git brew.
+# Remaining work: dependency check. location of git install.
+
+sudo apt-get install pioneers
+git clone https://github.com/farin/JCloisterZone.git
+brew install spaceinvaders-go
+brew install yetris
