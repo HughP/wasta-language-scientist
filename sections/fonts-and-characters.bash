719346591a37220c0e0c969ad59a7ec7db3294ef
HughP <HughP@users.noreply.github.com>
2017-02-11 12:57:27 +0100

    Split out of main script

---
+++ sections/fonts-and-characters.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+################################################################################
+# Script Name: fonts-and-characters.bash
+# Author: Hugh Paterson III <email here>
+# Version: 0.01
+# License: GPL
+
+### Let's install the KDE Character selector to find the right unicode character. The KDE interaction around character selection is not as clean or clear as on OS X, but is much better than the GNOME interface which ships with unity. ###
+
+sudo apt-get install kcharselect
