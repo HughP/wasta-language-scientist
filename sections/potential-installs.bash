26dc6e23dccc60cff8c0a66acb0cf7a2db3db770
HughP <HughP@users.noreply.github.com>
2017-02-13 22:44:39 +0100

    added a section of tools I would like to look at installing but am not currently in a position to download them or determin if they are the best suiting tool.

---
+++ sections/potential-installs.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+################################################################################
+# Script Name: potential-installs.bash
+# Author: Hugh Paterson III <email here>
+# Version: 0.01
+# License: GPL
+# Dependencies: None.
+
+# * https://github.com/fauno/jekyll-pandoc-multiple-formats #https://endefensadelsl.org/
+# * https://github.com/mfenner/jekyll-pandoc
+# * http://wasifbaig.com/brackets-zotero/ #Brackets Zotero Integration Extension
+# * https://github.com/davepwsmith/zotpick-applescript/
+# * http://davepwsmith.github.io/academic-scrivener-howto/
+# * https://github.com/KurtPfeifle/pandoc-csl-testing
+# * https://github.com/jgm/pandoc/wiki/Pandoc-wrappers-and-interfaces
+# * https://www.alfredforum.com/topic/4686-pandoctor-an-alfred-gui-for-pandoc/
+# * https://github.com/bk/pdc #pdc - pandoc wrapper
+# * https://github.com/ocharles/pandoc-live
+# * https://github.com/msprev/unite-bibtex
+# * https://github.com/vim-pandoc
+# * http://rmarkdown.rstudio.com/
+# * https://github.com/jgm/pandoc/wiki/Pandoc-Extras
+# * http://windowssecrets.com/forums/showthread.php/176508-PowerShell-GUI-front-end-for-PanDoc
+# * https://github.com/wcaleb/pandocket
+
+# * https://wiki.gnome.org/Apps/Dia/Download
+# * http://dia-installer.de/download/index.html
+# * http://www.graphviz.org/Download.php
+# * https://packages.debian.org/search?suite=all&searchon=names&keywords=graphviz
+# * or brew install graphviz
+
+
+# https://github.com/tlozoot/experigen for phonology experiments
+#https://github.com/cpill0789/experigen
+
+
+#QGIS plugins
+* https://github.com/sagost/VideoUavTracker
