aac75947df50d362d41f237d77357505a78f1501
HughP <HughP@users.noreply.github.com>
2017-02-27 21:51:15 +0100

    Added the Lang-tools folder to add more structure to the additions.

---
+++ Lang-Ling-tools/Color.bash
@@ -0,0 +1,3 @@
+
+http://sites.uci.edu/pruthadeshpande/files/2015/10/2015Dec04_IMBSworkshop.pdf
+http://www1.icsi.berkeley.edu/wcs/data.html
---
+++ Lang-Ling-tools/Keyboarding-reseach-programing.bash
@@ -0,0 +1,15 @@
+http://stackoverflow.com/questions/5044073/python-cross-platform-listening-for-keypresses
+http://stackoverflow.com/questions/10693256/how-to-accept-keypress-in-command-line-python
+
+http://stackoverflow.com/questions/7264208/how-to-use-python-curses-module-to-create-a-key-event/7264312#7264312
+http://stackoverflow.com/questions/24072790/detect-key-press-in-python
+http://stackoverflow.com/questions/17815686/detect-key-input-in-python
+http://www.pygame.org/docs/ref/event.html
+http://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user/21659588#21659588
+http://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user
+https://automatetheboringstuff.com/chapter15/
+
+
+https://gitlab.com/edimitchel/KeyboardEventHandler
+https://gitlab.com/npm-dom/key-event
+https://gitlab.com/johnantoni/jquery.simulate
